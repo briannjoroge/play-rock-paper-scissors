85898276e67d0c428d49bae352a5757d12163f6f
briannjoroge <briannjoroge007@gmail.com>
2025-05-25 19:48:01 +0300

    updates the player-choice emoji when a player selects a button

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const rockButton = document.querySelector(".rock-button");
 const paperButton = document.querySelector(".paper-button");
 const scissorsButton = document.querySelector(".scissors-button");
+const playerChoiceDisplay = document.getElementById("player-choice");
 
 rockButton.addEventListener("click", () => {
   console.log("You clicked ROCK!");
+  playerChoiceDisplay.textContent = "🤜🏼";
 });
 
 paperButton.addEventListener("click", () => {
   console.log("You clicked PAPER!");
+  playerChoiceDisplay.textContent = "🖐🏻";
 });
 
 scissorsButton.addEventListener("click", () => {
   console.log("You clicked SCISSORS!");
+  playerChoiceDisplay.textContent = "✌🏻";
 });
