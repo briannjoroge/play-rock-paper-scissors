72edeab7888be78f5ae627f8ae0ca29a52f5eb26
briannjoroge <briannjoroge007@gmail.com>
2025-05-25 20:45:33 +0300

    added the checkWinner function and updates the winner result

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const paperButton = document.querySelector(".paper-button");
 const scissorsButton = document.querySelector(".scissors-button");
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const playerChoiceDisplay = document.getElementById("player-choice");
+const winnerDisplay = document.querySelector(".winner-display h1");
 
 function getRandomComputerMove() {
   let moves = ["🤜🏼", "🖐🏻", "✌🏻"];
@@ -11,26 +12,54 @@ function getRandomComputerMove() {
   return selectedMove;
 }
 
+function checkWinner(playerMove, computerMove) {
+  if (playerMove === computerMove) {
+    return "Draw";
+  }
+
+  if (
+    (playerMove === "🤜🏼" && computerMove === "✌🏻") ||
+    (playerMove === "🖐🏻" && computerMove === "🤜🏼") ||
+    (playerMove === "✌🏻" && computerMove === "🖐🏻")
+  ) {
+    return "Player Wins";
+  }
+
+  return "Computer Wins";
+}
+
 rockButton.addEventListener("click", () => {
-  console.log("You clicked ROCK!");
+  //   console.log("You clicked ROCK!");
+  const playerMove = "🤜🏼";
   playerChoiceDisplay.textContent = "🤜🏼";
 
   const computerMove = getRandomComputerMove();
   computerChoiceDisplay.textContent = computerMove;
+
+  const result = checkWinner(playerMove, computerMove);
+  winnerDisplay.textContent = result;
 });
 
 paperButton.addEventListener("click", () => {
-  console.log("You clicked PAPER!");
+  //   console.log("You clicked PAPER!");
+  const playerMove = "🖐🏻";
   playerChoiceDisplay.textContent = "🖐🏻";
 
   const computerMove = getRandomComputerMove();
   computerChoiceDisplay.textContent = computerMove;
+
+  const result = checkWinner(playerMove, computerMove);
+  winnerDisplay.textContent = result;
 });
 
 scissorsButton.addEventListener("click", () => {
-  console.log("You clicked SCISSORS!");
+  //   console.log("You clicked SCISSORS!");
+  const playerMove = "✌🏻";
   playerChoiceDisplay.textContent = "✌🏻";
 
   const computerMove = getRandomComputerMove();
   computerChoiceDisplay.textContent = computerMove;
+
+  const result = checkWinner(playerMove, computerMove);
+  winnerDisplay.textContent = result;
 });
