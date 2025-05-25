1f14c665d14f44beb70abda9a5f035cffbfec676
briannjoroge <briannjoroge007@gmail.com>
2025-05-25 21:10:34 +0300

    updates the score board of both computer and player after winning

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const scissorsButton = document.querySelector(".scissors-button");
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const playerChoiceDisplay = document.getElementById("player-choice");
 const winnerDisplay = document.querySelector(".winner-display h1");
+const computerScoreSpan = document.getElementById("computer-score");
+const playerScoreSpan = document.getElementById("player-score");
+
+let playerScore = 0;
+let computerScore = 0;
 
 function getRandomComputerMove() {
   let moves = ["🤜🏼", "🖐🏻", "✌🏻"];
@@ -28,38 +33,57 @@ function checkWinner(playerMove, computerMove) {
   return "Computer Wins";
 }
 
+function updateScore(result) {
+  if (result === "Player Wins") {
+    playerScore++;
+    playerScoreSpan.textContent = playerScore;
+  } else if (result === "Computer Wins") {
+    computerScore++;
+    computerScoreSpan.textContent = computerScore;
+  }
+}
+
 rockButton.addEventListener("click", () => {
   //   console.log("You clicked ROCK!");
   const playerMove = "🤜🏼";
-  playerChoiceDisplay.textContent = "🤜🏼";
+  playerChoiceDisplay.textContent = playerMove;
 
   const computerMove = getRandomComputerMove();
   computerChoiceDisplay.textContent = computerMove;
 
   const result = checkWinner(playerMove, computerMove);
   winnerDisplay.textContent = result;
+
+  //update score
+  updateScore(result);
 });
 
 paperButton.addEventListener("click", () => {
   //   console.log("You clicked PAPER!");
   const playerMove = "🖐🏻";
-  playerChoiceDisplay.textContent = "🖐🏻";
+  playerChoiceDisplay.textContent = playerMove;
 
   const computerMove = getRandomComputerMove();
   computerChoiceDisplay.textContent = computerMove;
 
   const result = checkWinner(playerMove, computerMove);
   winnerDisplay.textContent = result;
+
+  //update score
+  updateScore(result);
 });
 
 scissorsButton.addEventListener("click", () => {
   //   console.log("You clicked SCISSORS!");
   const playerMove = "✌🏻";
-  playerChoiceDisplay.textContent = "✌🏻";
+  playerChoiceDisplay.textContent = playerMove;
 
   const computerMove = getRandomComputerMove();
   computerChoiceDisplay.textContent = computerMove;
 
   const result = checkWinner(playerMove, computerMove);
   winnerDisplay.textContent = result;
+
+  //update score
+  updateScore(result);
 });
